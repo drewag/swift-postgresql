6f75ec29acf59ae9493296f56c55e631c5110a5b
Andrew Wagner <845683+drewag@users.noreply.github.com>
2019-09-16 21:53:32 -0600

    [UP] Update to Swift 5.1

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/drewag/swift-sql.git", from: "4.0.0"),
+        .package(url: "https://github.com/drewag/swift-sql.git", from: "5.0.0"),
         .package(url: "https://github.com/vapor-community/cpostgresql.git", from: "2.0.0"),
     ],
     targets: [
